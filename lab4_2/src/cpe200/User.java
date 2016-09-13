e04896eb44d6bed202f558014e3993c469b8f101
Supapitch Thungtragul <bombubble@gmail.com>
2016-09-13 23:38:04 +0700

    Update User.java

--- lab4_2/src/cpe200/User.java
+++ lab4_2/src/cpe200/User.java
@@ -1,5 +1,10 @@
 package cpe200;
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Pattern;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.util.regex.Matcher;
+
 /**
  * Created by pruet on 6/9/2559.
  */
@@ -11,18 +16,28 @@ public class User {
     }
 
     public boolean setUserName(String name) {
+        if(name.length()>=8 && name.matches("(^[a-zA-Z]+)([a-zA-Z0-9]*)")){
+            this.userName = name;
+            return true;
+        }
         return false;
     }
 
     public boolean setPassword(String name) {
+        if(name.length()>=12 && name.matches("(.*)([A-Z]+)(.*)") && name.matches("(.*)([a-z]+)(.*)") && name.matches("(.*)([0-9]+)(.*)")){
+            this.password = name;
+            return true;
+        }
         return false;
     }
 
     public String getUserName() {
-        return null;
+
+        return userName;
     }
 
     public String getPassword() {
-        return null;
+
+        return password;
     }
 }
